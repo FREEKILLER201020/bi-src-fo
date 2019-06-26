73b711c78e208d8edbaac4c79ff106ef116c4f6e
Alex Titoff <rozdol@gmail.com>
2019-06-26 16:29:56 +0300

    CRUD for partners

--- actions/details/_init.php
+++ actions/details/_init.php
@@ -1,11 +1,11 @@
 <?php 
-if($this->data->field_exists($what, 'user_id')) {
-	$user_id=$this->data->get_val($what,'user_id',$id);
-	if($user_id!=$GLOBALS[uid])$this->html->error('Not allowed');
-	//echo $this->html->pre_display($user_id,"user_id");
+// if($this->data->field_exists($what, 'user_id')) {
+// 	$user_id=$this->data->get_val($what,'user_id',$id);
+// 	if($user_id!=$GLOBALS[uid])$this->html->error('Not allowed for this user');
+// 	//echo $this->html->pre_display($user_id,"user_id");
 
-}else{
-  	//$this->html->error('Not allowed');
-}
+// }else{
+//   	//$this->html->error('Not allowed');
+// }
 
 	
