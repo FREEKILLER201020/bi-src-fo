21e5903c02dc88e5ccbbd225b30e70ecf69fe638
freekiller201020 <htc.burchenko@gmail.com>
2019-08-06 16:41:00 +0300

    Updates files

--- updates/update_0002.php
+++ updates/update_0002.php
@@ -1,28 +1,33 @@
 <?php
-if (!$access['main_admin']) { //Show error if not admin
-    $this->html->error('');
+if (!$access['main_admin']) {
+	//Show error if not admin
+	$this->html->error('');
 }
-echo $this->html->message("Running update $update_version","");
+echo $this->html->message("Running update $update_version", "");
 
 $this->livestatus('DONE');
 $this->data->writeconfig('update_version', $update_version);
 
 //////===============================================\\\\\\\\
-$list_id=7;
-$sql="SELECT \"add_new_list\" ($list_id, 'Project Stages','stage',array['Planned','In progress','Completed']);";
+
+// Create Project stages list
+$list_id = 7;
+$sql = "SELECT \"add_new_list\" ($list_id, 'Project Stages','stage',array['Planned','In progress','Completed']);";
 $this->db->getVal($sql);
 
+// Create Project category list
+$list_id = 8;
+$sql = "SELECT \"add_new_list\" ($list_id, 'Project Category','category',array['Бизнес проекты/задачи','Недвижимость членов семьи','Налоговые вопросы и вопросы наследования','Операционная поддержка','Прочее']);";
+$this->db->getVal($sql);
 
 //////===============================================\\\\\\\\
 
 $update_version++;
 
-$update_version_fm=sprintf('%04d', $update_version);
-$update_file=APP_DIR.DS.'updates'.DS."update_".$update_version_fm. '.php';
+$update_version_fm = sprintf('%04d', $update_version);
+$update_file = APP_DIR . DS . 'updates' . DS . "update_" . $update_version_fm . '.php';
 if (file_exists($update_file)) {
-    require $update_file;
-}else{
-    echo $this->html->message("Up to date","");
+	require $update_file;
+} else {
+	echo $this->html->message("Up to date", "");
 }
-
-
