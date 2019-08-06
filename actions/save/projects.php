1d79254b337eabe16b944840bdfffbf982d9dc57
Alex Titoff <rozdol@gmail.com>
2019-08-06 17:08:39 +0300

    Update projects.php

--- actions/save/projects.php
+++ actions/save/projects.php
@@ -1,21 +1,25 @@
 <?php
 //Save projects
 $id = $this->html->readRQn('id');
-$user_id = $this->html->readRQn('user_id');
+
 $name = $this->html->readRQ('name');
-$date = $this->html->readRQ('date');
-$date_from = $this->html->readRQ('date_from');
-$date_to = $this->html->readRQ('date_to');
-$date_check = $this->html->readRQ('date_check');
+
+$date_from = $this->html->readRQd('date_from',1);
+$date_to = $this->html->readRQd('date_to');
+$date_check = $this->html->readRQd('date_check');
 $active = $this->html->readRQn('active');
 $stage_id = $this->html->readRQn('stage_id');
 $category_id = $this->html->readRQn('category_id');
 $descr = $this->html->readRQ('descr');
 
+
+if($name=='')$name=$this->data->get_new_name($what, $GLOBALS[today],'','PRJ-');
+if($date_to=='')$date_to=$this->dates->F_dateadd_month($date_from,3);
+if($date_check=='')$date_check=$this->dates->F_dateadd($date_to,-7);
+
+
 $vals = array(
-	'user_id' => $user_id,
 	'name' => $name,
-	'date' => $date,
 	'date_from' => $date_from,
 	'date_to' => $date_to,
 	'date_check' => $date_check,
@@ -24,6 +28,12 @@ $vals = array(
 	'category_id' => $category_id,
 	'descr' => $descr,
 );
+if($id == 0){
+	$vals[date]=$GLOBALS[today];
+	$vals[user_id]=$GLOBALS[uid];
+
+}
+
 echo $this->html->pre_display($_POST, 'Post');
 echo $this->html->pre_display($vals, 'Vals');exit;
 if ($id == 0) {$id = $this->db->insert_db($what, $vals);} else { $id = $this->db->update_db($what, $id, $vals);}
