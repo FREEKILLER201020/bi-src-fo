4d1c2f9af83e9c0cd731af86e05ba32c2694097d
freekiller201020 <htc.burchenko@gmail.com>
2019-08-06 17:08:28 +0300

    Update projects.php

--- actions/form/projects.php
+++ actions/form/projects.php
@@ -23,11 +23,10 @@ $out .= $this->html->form_hidden('refid', $refid);
 // $user_id = $this->data->listitems('user_id', $res[user_id], 'user', 'span12');
 // $out .= "<label>User</label>$user_id";
 $out .= $this->html->form_text('name', $res[name], 'Name', '', 0, 'span12');
-$out .= $this->html->form_date('date', $res[date], 'Date', '', 0, 'span12');
+// $out .= $this->html->form_date('date', $res[date], 'Date', '', 0, 'span12');
 $out .= $this->html->form_date('date_from', $res[date_from], 'Date from', '', 0, 'span12');
-$out .= $this->html->form_date('date_to', $res[date_to], 'Date to', '', 0, 'span12');
 $out .= $this->html->form_date('date_check', $res[date_check], 'Date check', '', 0, 'span12');
-$out .= $this->html->form_chekbox('active', $res[active], 'Active', '', 0, 'span12');
+$out .= $this->html->form_date('date_to', $res[date_to], 'Date to', '', 0, 'span12');
 
 // $stage_id = $this->data->listitems('stage_id', $res[stage_id], 'stage', 'span12','Select from list');
 // $out .= "<label>Stage</label>$stage_id";
@@ -43,6 +42,7 @@ $out .= "<label>Category</label>$category_id";
 $out .= $this->html->htlist('category_id', $sql, $res[category_id], 'Select Category', "", '', 'span12');
 
 $out .= $this->html->form_textarea('descr', $res[descr], 'Descr', '', 0, '', 'span12');
+$out .= $this->html->form_chekbox('active', $res[active], 'Active', '', 0, 'span12');
 $out .= $this->html->form_confirmations();
 $out .= $this->html->form_submit('Save');
 $out .= $this->html->form_end();
