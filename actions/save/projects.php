ec3ab152dd26a14c2e1d65969e9ce38676cf6ee3
freekiller201020 <htc.burchenko@gmail.com>
2019-08-08 14:41:27 +0300

    Update projects.php

--- actions/save/projects.php
+++ actions/save/projects.php
@@ -4,7 +4,7 @@ $id = $this->html->readRQn('id');
 
 $name = $this->html->readRQ('name');
 
-$date_from = $this->html->readRQd('date_from',1);
+$date_from = $this->html->readRQd('date_from', 1);
 $date_to = $this->html->readRQd('date_to');
 $date_check = $this->html->readRQd('date_check');
 $active = $this->html->readRQn('active');
@@ -12,13 +12,25 @@ $stage_id = $this->html->readRQn('stage_id');
 $category_id = $this->html->readRQn('category_id');
 $descr = $this->html->readRQ('descr');
 
+if ($name == '') {
+	$name = $this->data->get_new_name($what, $GLOBALS[today], '', 'PRJ-');
+}
 
-if($name=='')$name=$this->data->get_new_name($what, $GLOBALS[today],'','PRJ-');
-if($date_to=='')$date_to=$this->dates->F_dateadd_month($date_from,3);
-if($date_check=='')$date_check=$this->dates->F_dateadd($date_to,-7);
+if ($date_to == '') {
+	$date_to = $this->dates->F_dateadd_month($date_from, 3);
+}
 
-if($this->dates->is_later($date_check,$date_to))$this->html->error('Date check is later than comletion date');
+if ($date_check == '') {
+	$date_check = $this->dates->F_dateadd($date_to, -7);
+}
 
+if ($this->dates->is_later($date_check, $date_to)) {
+	$this->html->error('Date check is later than comletion date');
+}
+
+if ($this->dates->is_later($date_from, $date_to)) {
+	$this->html->error('Date from is later than comletion date');
+}
 
 $vals = array(
 	'name' => $name,
@@ -30,9 +42,9 @@ $vals = array(
 	'category_id' => $category_id,
 	'descr' => $descr,
 );
-if($id == 0){
-	$vals[date]=$GLOBALS[today];
-	$vals[user_id]=$GLOBALS[uid];
+if ($id == 0) {
+	$vals[date] = $GLOBALS[today];
+	$vals[user_id] = $GLOBALS[uid];
 
 }
 
